243fe45506e5395412d1f87cd4ecec14331bcc3f
skhandayan <handayan.seankirk1@gmail.com>
2025-08-16 14:23:25 +0800

    Angular Directives

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,44 +1,57 @@
 import { Component} from '@angular/core';
+import {JsonPipe, NgStyle, NgClass} from "@angular/common"
+
+
+interface Post {
+  id: number;
+  postTitle: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe, NgStyle, NgClass],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
 
 export class App {
+  
 
-  postTitle?: string;
-  postDetails?: string;
-  postImageUrl?: string;
-  postUrl?: string;
-  isChecked?: boolean;
+  postArray: Array<string> = ['Post 1', 'Post 2', 'Post 3', 'Post 4', 'Post 5']
+  
+  objectArray: Array<Post> = [
+    {
+      id: 1, 
+      postTitle: 'POST 1'
+    }, {
+      id: 2, 
+      postTitle: 'POST 2'
+    }, {
+      id: 3, 
+      postTitle: 'POST 3'
+    }, {
+      id: 4, 
+      postTitle: 'POST 4'
+    }, {
+      id: 5, 
+      postTitle: 'POST 5'
+    }
+  ]
 
-  getTitle($event: Event) {
-    const input = $event.target as HTMLInputElement;
-    this.postTitle = input.value
+  stepForm?: string;
+
+  isActive: boolean = true;
+
+  addNew() {
+    this.objectArray.push({id: 6, postTitle: 'POST 6'});
   }
 
-  getDetails($event: Event) {
-    const input = $event.target as HTMLInputElement;
-    this.postDetails = input.value
+  removeItem(index: number) {
+    this.objectArray.splice(index, 1)
   }
 
-  getImageUrl($event: Event) {
-    const input = $event.target as HTMLInputElement;
-    this.postImageUrl = input.value
+  onClick(status: string) {
+    this.stepForm = status
   }
-
-  getUrl($event: Event) {
-    const input = $event.target as HTMLInputElement;
-    this.postUrl = input.value
-  }
-
-  toggleCheckBox($event: Event) {
-    const isChecked = $event.target as HTMLInputElement;
-    this.isChecked = isChecked.checked
-  }
-
 }
