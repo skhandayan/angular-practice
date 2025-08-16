94f009471b508c7c607a633dc0e9db3e5d229444
skhandayan <handayan.seankirk1@gmail.com>
2025-08-16 17:49:53 +0800

    Completed Angular Task 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,57 +1,33 @@
 import { Component} from '@angular/core';
-import {JsonPipe, NgStyle, NgClass} from "@angular/common"
+import { FormsModule } from '@angular/forms';
 
-
-interface Post {
-  id: number;
-  postTitle: string;
-}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [JsonPipe, NgStyle, NgClass],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
 
 export class App {
-  
+    
+  name: string = '';
+  email: string = '';
+  address: string = '';
 
-  postArray: Array<string> = ['Post 1', 'Post 2', 'Post 3', 'Post 4', 'Post 5']
-  
-  objectArray: Array<Post> = [
-    {
-      id: 1, 
-      postTitle: 'POST 1'
-    }, {
-      id: 2, 
-      postTitle: 'POST 2'
-    }, {
-      id: 3, 
-      postTitle: 'POST 3'
-    }, {
-      id: 4, 
-      postTitle: 'POST 4'
-    }, {
-      id: 5, 
-      postTitle: 'POST 5'
-    }
-  ]
+  userDetailsArray: Array<any> = [];
 
-  stepForm?: string;
-
-  isActive: boolean = true;
-
-  addNew() {
-    this.objectArray.push({id: 6, postTitle: 'POST 6'});
+  saveNewData() {
+    this.userDetailsArray.push({
+      name: this.name,
+      email: this.email,
+      address: this.address
+    });
+    console.log(this.userDetailsArray)
   }
 
   removeItem(index: number) {
-    this.objectArray.splice(index, 1)
-  }
-
-  onClick(status: string) {
-    this.stepForm = status
+    this.userDetailsArray.splice(index, 1)
   }
 }
