568cb631516d1acf1121a37ff75c0edd8568f653
skhandayan <handayan.seankirk1@gmail.com>
2025-08-16 11:45:36 +0800

    Completed Task 01

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,4 +10,35 @@ import { Component} from '@angular/core';
 
 export class App {
 
+  postTitle?: string;
+  postDetails?: string;
+  postImageUrl?: string;
+  postUrl?: string;
+  isChecked?: boolean;
+
+  getTitle($event: Event) {
+    const input = $event.target as HTMLInputElement;
+    this.postTitle = input.value
+  }
+
+  getDetails($event: Event) {
+    const input = $event.target as HTMLInputElement;
+    this.postDetails = input.value
+  }
+
+  getImageUrl($event: Event) {
+    const input = $event.target as HTMLInputElement;
+    this.postImageUrl = input.value
+  }
+
+  getUrl($event: Event) {
+    const input = $event.target as HTMLInputElement;
+    this.postUrl = input.value
+  }
+
+  toggleCheckBox($event: Event) {
+    const isChecked = $event.target as HTMLInputElement;
+    this.isChecked = isChecked.checked
+  }
+
 }
