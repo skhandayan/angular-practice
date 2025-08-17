d3101d539cf723cd3892aaae6745fb5362f1fc83
skhandayan <handayan.seankirk1@gmail.com>
2025-08-17 11:15:43 +0800

    Angular Service Inital commit

--- src/app/Pipes/append-cli-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AppendCLIPipe } from './append-cli-pipe';
-
-describe('AppendCLIPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AppendCLIPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
